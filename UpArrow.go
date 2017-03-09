cc8f69b4fa2da4b5ff29526d33e2b2fefd8249cc
Marcel Rösler <roesler1@gmx.de>
2017-03-09 17:32:37 +0100

    prevent negativ exponent

--- UpArrow.go
+++ UpArrow.go
@@ -3,13 +3,13 @@ package lib
 import "math"
 
 /*UpArrow ...*/
-func UpArrow(base int64, exponant int64, upArrowAmount int64) int64 {
+func UpArrow(base uint64, exponant uint64, upArrowAmount uint64) uint64 {
 	if upArrowAmount <= 0 {
-		return int64(base * exponant)
+		return uint64(base * exponant)
 	} else if upArrowAmount == 1 {
-		return int64(math.Pow(float64(base), float64(exponant)))
-	} else if exponant == 1 {
-		return int64(base)
+		return uint64(math.Pow(float64(base), float64(exponant)))
+	} else if exponant <= 1 {
+		return uint64(base)
 	}
 	return UpArrow(base, UpArrow(base, exponant-1, upArrowAmount), upArrowAmount-1)
 }
