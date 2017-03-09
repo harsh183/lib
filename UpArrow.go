a18c7599a56da10d8b397cd6031652f055af1e20
Marcel Rösler <roesler1@gmx.de>
2017-03-09 17:28:05 +0100

    prevent negativ uparrow

--- UpArrow.go
+++ UpArrow.go
@@ -4,7 +4,7 @@ import "math"
 
 /*UpArrow ...*/
 func UpArrow(base int64, exponant int64, upArrowAmount int64) int64 {
-	if upArrowAmount == 0 {
+	if upArrowAmount <= 0 {
 		return int64(base * exponant)
 	} else if upArrowAmount == 1 {
 		return int64(math.Pow(float64(base), float64(exponant)))
